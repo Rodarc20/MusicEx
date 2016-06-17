df1032276344b25d2935593fdbb70f7d3e8da8bc
Rodrigo Gutierrez <rodrigo.gutierrez.k@gmail.com>
2016-06-17 17:07:02 -0500

    Initial commit

---
+++ src/main/java/com/example/MusicExApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicExApplication.class, args);
+	}
+}
