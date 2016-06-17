cd3b1040f12540fd2a042457ef9de2f9a0412dea
Rodrigo Gutierrez <rodrigo.gutierrez.k@gmail.com>
2016-06-17 17:59:36 -0500

    Agregado parametros de base de datos y la clase cancion del paquete dominio

---
+++ src/main/java/com/example/dominio/Cancion.java
@@ -0,0 +1,109 @@
+package com.example.dominio;
+
+/**
+ * Created by rodrigo on 17/06/16.
+ */
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.SequenceGenerator;
+import javax.swing.text.StringContent;
+
+@Entity
+public class Cancion {
+    @Id
+    @SequenceGenerator(name="Cancion_ID_GENERATOR", sequenceName="Cancion_ID_SEQ")
+    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="Cancion_ID_GENERATOR")
+    private Integer id_cancion;
+    private String titulo;
+    private String path;
+    private Integer numero;
+    private Integer id_album;
+    //private Integer id_artista;//quiza no sea necesario
+    private Integer duracion;
+    private Integer numero_seguidores;
+    private Integer puntuacion;
+    private Integer reproducciones;
+
+    public void setTitulo(String titulo){
+        this.titulo = titulo;
+    }
+
+    public String getTitulo(){
+        return this.titulo;
+    }
+
+    public Integer getId_cancion(){
+        return this.id_cancion;
+    }
+
+    public void setPath(String path){
+        this.path = path;
+    }
+
+    public String getPath(){
+        return this.path;
+    }
+
+    public void setNumero(Integer numero){
+        this.numero = numero;
+    }
+
+    public Integer getNumero(){
+        return this.numero;
+    }
+
+    public void setId_album(Integer id_album){
+        this.id_album = id_album;
+    }
+
+    public Integer getId_album(){
+        return this.id_album;
+    }
+
+    public void setDuracion(Integer duracion){
+        this.duracion = duracion;
+    }
+
+    public Integer getDuracion(){
+        return this.duracion;
+    }
+
+    public void setNumero_seguidores(Integer numero_seguidores){
+        this.numero_seguidores = numero_seguidores;
+    }
+
+    public Integer getNumero_seguidores(){
+        return this.numero_seguidores;
+    }
+
+    public void setPuntuacion(Integer puntuacion){
+        this.puntuacion = puntuacion;
+    }
+
+    public Integer getPuntuacion(){
+        return this.puntuacion;
+    }
+
+    public void setReproducciones(Integer reproducciones){
+        this.reproducciones= reproducciones;
+    }
+
+    public Integer getReproducciones(){
+        return this.reproducciones;
+    }
+    public Cancion(){}
+
+    public Cancion(String titulo, String path, Integer numero, Integer id_album, Integer duracion){
+        this.titulo = titulo;
+        this.path = path;
+        this.numero = numero;
+        this.id_album = id_album;
+        this.duracion = duracion;
+        this.numero_seguidores = 0;
+        this.puntuacion = 0;
+        this.reproducciones = 0;
+    }
+}
