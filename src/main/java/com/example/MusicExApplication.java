d82500e69ab21fbab28d11d2285b4e910b639ac3
Rodrigo Gutierrez <rodrigo.gutierrez.k@gmail.com>
2016-06-17 19:28:48 -0500

    agregado el controlador y propiedades para base de datos

--- src/main/java/com/example/MusicExApplication.java
+++ src/main/java/com/example/MusicExApplication.java
@@ -1,11 +1,17 @@
 package com.example;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.orm.jpa.EntityScan;
 
+import com.example.dominio.Artista;
+import com.example.dominio.Album;
+import com.example.dominio.Cancion;
+
 @EnableAutoConfiguration
+@EntityScan(basePackageClasses = Artista.class)
 @SpringBootApplication
 public class MusicExApplication {
 
