525a306009b92ac4e875c15dda5179a449ea255f
Rodrigo Gutierrez <rodrigo.gutierrez.k@gmail.com>
2016-06-17 19:04:40 -0500

    agregados los repositorios de cancion, album, artista

---
+++ src/main/java/com/example/repositorio/AlbumRepositorio.java
@@ -0,0 +1,12 @@
+package com.example.repositorio;
+
+/**
+ * Created by rodrigo on 17/06/16.
+ */
+import java.util.List;
+import org.springframework.data.repository.CrudRepository;
+import com.example.dominio.Album;
+
+public interface AlbumRepositorio extends CrudRepository<Album, Integer>{
+    List<Album> findAll();
+}
---
+++ src/main/java/com/example/repositorio/ArtistaRepositorio.java
@@ -0,0 +1,13 @@
+package com.example.repositorio;
+
+/**
+ * Created by rodrigo on 17/06/16.
+ */
+import java.util.List;
+import org.springframework.data.repository.CrudRepository;
+import com.example.dominio.Artista;
+
+public interface ArtistaRepositorio extends CrudRepository<Artista, Integer>{
+    List<Artista> findAll();
+    Artista findById_artista(Integer id_artista);
+}
